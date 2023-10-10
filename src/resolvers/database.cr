1d8047f1fd3450ebabfcf3266e9e439f4b456b4b
George Dietrich <george@dietrich.app>
2023-10-09 22:13:29 -0400

    Bump deps Fix compiler error

--- src/resolvers/database.cr
+++ src/resolvers/database.cr
@@ -10,16 +10,18 @@ struct Blog::Resolvers::Database
 
   # :inherit:
   def resolve(request : ATH::Request, parameter : ATH::Controller::ParameterMetadata(E)) : E? forall E
-    return unless parameter.annotation_configurations.has? Entity
+    {% if E < Blog::Entities::Entity %}
+      return unless parameter.annotation_configurations.has? Entity
 
-    id = request.attributes.get "id", Int64
+      id = request.attributes.get "id", Int64
 
-    unless entity = @entity_manager.repository(E).find? id
-      Log.context.set entity_class: E.name, id: id
-      raise ATH::Exceptions::NotFound.new "An item with the provided ID could not be found."
-    end
+      unless entity = @entity_manager.repository(E).find? id
+        Log.context.set entity_class: E.name, id: id
+        raise ATH::Exceptions::NotFound.new "An item with the provided ID could not be found."
+      end
 
-    entity
+      entity
+    {% end %}
   end
 end
 
