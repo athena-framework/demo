d92e69c5ae05073cebef4226311e2546c9f401ca
George Dietrich <dev@dietrich.pub>
2024-02-22 18:59:30 -0500

    Dep updates (#12)
    * Upgrade `micrate` to avoid needing to set `DATABASE_URL` all the time

--- src/commands/db_migrate.cr
+++ src/commands/db_migrate.cr
@@ -4,8 +4,6 @@ require "micrate"
 @[ADI::Register]
 class Blog::Commands::DBMigrate < ACON::Command
   protected def execute(input : ACON::Input::Interface, output : ACON::Output::Interface) : ACON::Command::Status
-    # TODO: Remove this once https://github.com/amberframework/micrate/pull/88 is resolved.
-    Micrate.connection_url = ENV["DATABASE_URL"]
     Micrate::Cli.run_up
 
     Status::SUCCESS
